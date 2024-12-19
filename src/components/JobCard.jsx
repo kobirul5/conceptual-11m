9d98aaac82111e4be367f1059edd6a14387e51aa
kobirul5 <kobirul05j@gmail.com>
2024-12-19 08:44:45 +0600

    added new page

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from 'react-router-dom'
+import {format} from 'date-fns';
 
 const JobCard = ({job}) => {
   const  {job_title,category, deadline, min_price, max_price, description, bid_count} = job
@@ -11,7 +12,7 @@ const JobCard = ({job}) => {
     >
       <div className='flex items-center justify-between'>
         <span className='text-xs font-light text-gray-800 '>
-          {deadline}
+          Deadline: {format(new Date(deadline), 'P')}
         </span>
         <span className='px-3 py-1 text-[8px] text-blue-800 uppercase bg-blue-200 rounded-full '>
           {category}
